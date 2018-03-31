f2a636be4d2d4810bb59d0ee8b942d3f0f869513
Ilya Shmalko <ilasmalko@gmail.com>
2018-03-31 17:38:10 +0300

    Четвертый урок/усложненое

---
+++ lesson-4/hard/script.js
@@ -0,0 +1,21 @@
+let num = Number(prompt('Сколько массивов включить во внутрь arr?'));
+let arr = new Array(num);
+
+for (let i = 0; i < arr.length; i++) {
+
+    arr[i] = new Array(5);
+    for ( let j = 0; j < 5; j++) {
+
+    	arr[i][j] = Math.floor(Math.random()*99);
+    }
+}
+
+function arrSum(array, i = 0) {
+  if (isFinite(array))
+    return Number(array);
+  else if (typeof array == "object" && i < array.length)
+    return arrSum(array[i]) + arrSum(array, i + 1);
+  return 0;
+}
+
+alert(arrSum(arr));
