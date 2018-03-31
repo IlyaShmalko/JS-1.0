6f1075e191b09d42e2cf600916612f7eee3700ea
Ilya Shmalko <ilasmalko@gmail.com>
2018-03-31 17:42:38 +0300

    Четвертый урок/усложненое

--- lesson-4/hard/script.js
+++ lesson-4/hard/script.js
@@ -1,21 +1,25 @@
-let num = Number(prompt('Сколько массивов включить во внутрь arr?'));
-let arr = new Array(num);
+function matrix() {
+	let num = Number(prompt('Сколько массивов включить во внутрь arr?'));
+	let arr = new Array(num);
 
-for (let i = 0; i < arr.length; i++) {
+	for (let i = 0; i < arr.length; i++) {
 
-    arr[i] = new Array(5);
-    for ( let j = 0; j < 5; j++) {
+	    arr[i] = new Array(5);
+	    for ( let j = 0; j < 5; j++) {
 
-    	arr[i][j] = Math.floor(Math.random()*99);
-    }
+	    	arr[i][j] = Math.floor(Math.random()*99);
+	    }
+	}
+
+	function arrSum(array, i = 0) {
+	  if (isFinite(array))
+	    return Number(array);
+	  else if (typeof array == "object" && i < array.length)
+	    return arrSum(array[i]) + arrSum(array, i + 1);
+	  return 0;
+	}
+
+	alert(arrSum(arr));
 }
 
-function arrSum(array, i = 0) {
-  if (isFinite(array))
-    return Number(array);
-  else if (typeof array == "object" && i < array.length)
-    return arrSum(array[i]) + arrSum(array, i + 1);
-  return 0;
-}
-
-alert(arrSum(arr));
+matrix();
